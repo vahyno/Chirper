e0099f47ae22ea51490a2abe58709175b6461d89
vahyno <vahyno@gmail.com>
2019-03-13 16:02:06 -0700

    First Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+import { authedUser } from './authedUser';
+import { tweets } from './tweets';
+import { users } from './users';
+
+import { loadingBarReducer } from 'react-redux-loading-bar'
+
+export default combineReducers({
+    authedUser,
+    tweets,
+    users,
+    loadingBar: loadingBarReducer,
+});
