9f3b4f68c32c90393f6be92b07566f9a6e6ceeae
vahyno <vahyno@gmail.com>
2019-03-13 16:20:54 -0700

    provider, store, middleware, reducer to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import middleware from './middleware';
+import reducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
